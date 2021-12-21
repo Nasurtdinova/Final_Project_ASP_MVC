3d07fd8fcaaa38a6ad5377691e8f93a7daeba7c5
Гузель Насуртдинова <nasurtdinovag@mail.ru>
2021-12-21 14:59:59 +0300

    Add console project

---
+++ Console_Project/Program.cs
@@ -0,0 +1,22 @@
+﻿using Final_Project_ASP_MVC.Core;
+using System;
+using System.Collections.Generic;
+
+namespace Console_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Sportsman> sportsmans = new List<Sportsman>();
+            sportsmans = Connection.GetSportsmans();
+            foreach (Sportsman i in sportsmans)
+            {
+                
+                Console.WriteLine(i.Name);
+            }
+           
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
