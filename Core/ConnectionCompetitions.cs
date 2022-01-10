1b67e9557b60657d749817f547ca1e1124e1c460
Гузель Насуртдинова <nasurtdinovag@mail.ru>
2022-01-10 19:20:25 +0300

    Update competition

--- Core/ConnectionCompetitions.cs
+++ Core/ConnectionCompetitions.cs
@@ -1,114 +1,109 @@
-﻿using Final_Project_ASP_MVC.Core;
+﻿using Dapper;
+using Final_Project_ASP_MVC.Core;
+using Final_Project_ASP_MVC.Models;
 using System;
 using System.Collections.Generic;
-using MySql.Data.MySqlClient;
 using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
 
 namespace Core
 {
     public class ConnectionCompetitions
     {
-        private static string connStr = ConfigurationManager.AppSettings["connection"].ToString();
-        private static MySqlConnection conn;
+        private static string connStr = ConfigurationManager.ConnectionStrings["Competition"].ConnectionString;
+        private static IDbConnection connection = new SqlConnection(connStr);
 
         public static Competition GetCompetId(int id)
         {
-            conn.Open();
             Competition compet = null;
             try
             {
-                string sql = $"SELECT idCompetition,Competition.Competition.Name,NameVenue,Street,Home,Date, Competition.City.Name FROM Competition.Competition  join Competition.City  on Competition.Competition.idCity = Competition.City.idCity where idCompetition ={id};";
-                MySqlCommand cmd = new MySqlCommand(sql, conn);
-                MySqlDataReader res = cmd.ExecuteReader();
-
-                while (res.Read())
+                compet = new Competition
                 {
-                    compet = new Competition { ID = Convert.ToInt32(res[0]), Name = res[1].ToString(), NameVenue = res[2].ToString(), Street = res[3].ToString(), Home = Convert.ToInt32(res[4]), Date = Convert.ToDateTime(res[5]), City = res[6].ToString() };
-                }
-                res.Close();
+                    ID = connection.Query<int>($"SELECT idCompetition FROM Competition join City on Competition.idCity = City.idCity where idCompetition ={id};").AsList().FirstOrDefault(),
+                    Name = connection.Query<string>($"SELECT Competition.Name FROM Competition join City on Competition.idCity = City.idCity where idCompetition ={id};").AsList().FirstOrDefault(),
+                    NameVenue = connection.Query<string>($"SELECT NameVenue FROM Competition join City on Competition.idCity = City.idCity where idCompetition ={id};").AsList().FirstOrDefault(),
+                    Street = connection.Query<string>($"SELECT Street FROM Competition join City on Competition.idCity = City.idCity where idCompetition ={id};").AsList().FirstOrDefault(),
+                    Home = connection.Query<int>($"SELECT Home FROM Competition join City on Competition.idCity = City.idCity where idCompetition ={id};").AsList().FirstOrDefault(),
+                    Date = connection.Query<DateTime>($"SELECT Date FROM Competition join City on Competition.idCity = City.idCity where idCompetition ={id};").AsList().FirstOrDefault(),
+                    City = connection.Query<string>($"SELECT City.Name FROM Competition join City on Competition.idCity = City.idCity where idCompetition ={id};").AsList().FirstOrDefault()
+                };
             }
 
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
-            conn.Close();
+
             return compet;
         }
 
         public static List<Competition> GetCompetition()
         {
-            conn = new MySqlConnection(connStr);
-            conn.Open();
             List<Competition> competition = new List<Competition>();
 
             try
             {
-                string sql = "select idCompetition, Competition.Competition.Name, NameVenue, Street, Competition.City.Name, Home,Date from Competition.Competition join Competition.City on Competition.idCity = Competition.City.idCity;";
-                MySqlCommand cmd = new MySqlCommand(sql, conn);
-                MySqlDataReader res = cmd.ExecuteReader();
-
-                while (res.Read())
+                for (int i = 0; i < connection.Query<int>($"SELECT * FROM Competition;").Count(); i++)
                 {
-                    competition.Add(new Competition { ID = Convert.ToInt32(res[0]), Name = res[1].ToString(), NameVenue = res[2].ToString(), Street = res[3].ToString(), City = res[4].ToString(), Home = Convert.ToInt32(res[5].ToString()), Date = Convert.ToDateTime(res[6].ToString()) });
+                    competition.Add(new Competition
+                    {
+                        ID = connection.Query<int>("select idCompetition from Competition join City on Competition.idCity = City.idCity;").AsList()[i],
+                        Name = connection.Query<string>("select Competition.Name from Competition join City on Competition.idCity = City.idCity;").AsList()[i],
+                        NameVenue = connection.Query<string>("select NameVenue from Competition join City on Competition.idCity = City.idCity;").AsList()[i],
+                        Street = connection.Query<string>("select Street from Competition join City on Competition.idCity = City.idCity;").AsList()[i],
+                        City = connection.Query<string>("select City.Name from Competition join City on Competition.idCity = City.idCity;").AsList()[i],
+                        Home = connection.Query<int>("select Home from Competition join City on Competition.idCity = City.idCity;").AsList()[i],
+                        Date = connection.Query<DateTime>("select Date from Competition join City on Competition.idCity = City.idCity;").AsList()[i]
+                    });
                 }
-                res.Close();
             }
 
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
-            conn.Close();
+
             return competition;
         }
 
         public static void AddCompetition(Competition compet)
         {
-            conn = new MySqlConnection(connStr);
-            conn.Open();
             try
             {
-                MySqlCommand cmd = new MySqlCommand($"INSERT INTO `Competition`.`Competition` (`Name`, `NameVenue`, `Street`,Home,Date,idCity) SELECT '{compet.Name}', '{compet.NameVenue}', '{compet.Street}', {compet.Home}, '{compet.Date.ToString("yyyy-MM-dd hh:mm:ss")}', idCity  FROM Competition.City where '{compet.City}' = Competition.City.Name;", conn);
-                cmd.ExecuteNonQuery();
+                connection.Query($"INSERT INTO Competition (Name, NameVenue, Street,Home,Date,idCity) SELECT '{compet.Name}', '{compet.NameVenue}', '{compet.Street}', {compet.Home}, '{compet.Date.ToString("yyyy-MM-dd hh:mm:ss")}', idCity FROM City where '{compet.City}' = City.Name;");
+
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
-            conn.Close();
         }
 
         public static void RemoveCompetition(int id)
         {
-            conn = new MySqlConnection(connStr);
-            conn.Open();
             try
             {
-                MySqlCommand cmd = new MySqlCommand($"DELETE from Competition.Competition WHERE (idCompetition = '{id}')", conn);
-                cmd.ExecuteNonQuery();
+                connection.Query($"DELETE from Competition WHERE (idCompetition = '{id}')");
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
-            conn.Close();
         }
 
         public static void UpdateCompet(Competition compet)
         {
-            conn = new MySqlConnection(connStr);
-            conn.Open();
             try
             {
-                MySqlCommand cmd = new MySqlCommand($"update Competition.Competition set Name='{compet.Name}',NameVenue='{compet.NameVenue}',Street='{compet.Street}', Home='{compet.Home}',idCity = (select idCity from City where Competition.City.Name = '{compet.City}'), Date ='{compet.Date.ToString("yyyy-MM-dd hh:mm:ss")}' where idCompetition = {compet.ID};", conn);
-                cmd.ExecuteNonQuery();
+                connection.Query($"update Competition set Competition.Name ='{compet.Name}',NameVenue='{compet.NameVenue}',Street='{compet.Street}', Home='{compet.Home}',idCity = (select idCity from City where City.Name = '{compet.City}'), Date ='{compet.Date.ToString("yyyy-MM-dd hh:mm:ss")}' where idCompetition = {compet.ID};");
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
-            conn.Close();
         }
     }
 }
