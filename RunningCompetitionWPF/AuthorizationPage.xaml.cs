5f1e69bd6afcdbf301475fb1a3475b0b3a7517ec
Гузель Насуртдинова <nasurtdinovag@mail.ru>
2022-03-17 20:22:36 +0300

    Add authorization page

---
+++ RunningCompetitionWPF/AuthorizationPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CoreFramework;
+
+namespace RunningCompetitionWPF
+{
+    public partial class AuthorizationPage : Page
+    {
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void brnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (Connection.IsLogin(txt_login.Text,txt_password.Password) == 1)
+            {
+
+             }
+            else if (Connection.IsLogin(txt_login.Text, txt_password.Password) == 2)
+            {
+
+            }
+            else if (Connection.IsLogin(txt_login.Text, txt_password.Password) == 3)
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Incorrect login or password");
+            }
+        }
+    }
+}
